e850e7d826d88b80b0ad1430599507305d75d236
焦宏伟 <jiao.hongwei@qq.com>
2019-03-19 18:17:57 +0800

    Spring Cloud Bus 消息总线

--- commonservice-config-client/src/main/java/com/hw/commonserviceconfigclient/controller/ConfigController.java
+++ commonservice-config-client/src/main/java/com/hw/commonserviceconfigclient/controller/ConfigController.java
@@ -6,7 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @Description TODO
+ * @Description
+ *  配置文件修改自动刷新
+ *  1、添加rabbitmq
+ *  2、添加注解 @RefreshScope
+ *  3、修改git 配置文件上传
+ *  4、post 访问 http://localhost:8882/actuator/bus-refresh
+ *  5、刷新页面 http://localhost:8882/user
  * @Author hw
  * @Date 2019/3/19 17:43
  * @Version 1.0
