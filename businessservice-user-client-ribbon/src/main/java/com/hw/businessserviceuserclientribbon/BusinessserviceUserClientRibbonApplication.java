3f73fc13be394a9a8dc9189318f078d0efd6e73b
焦宏伟 <jiao.hongwei@qq.com>
2019-03-20 15:59:07 +0800

    Hystrix Turbine 断路器聚合监控

--- businessservice-user-client-ribbon/src/main/java/com/hw/businessserviceuserclientribbon/BusinessserviceUserClientRibbonApplication.java
+++ businessservice-user-client-ribbon/src/main/java/com/hw/businessserviceuserclientribbon/BusinessserviceUserClientRibbonApplication.java
@@ -2,17 +2,25 @@ package com.hw.businessserviceuserclientribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableTurbine
 @SpringBootApplication
 public class BusinessserviceUserClientRibbonApplication {
 
+//    http://localhost:8090/hystrix
+//    http://localhost:8090/turbine.stream
     public static void main(String[] args) {
         SpringApplication.run(BusinessserviceUserClientRibbonApplication.class, args);
     }
