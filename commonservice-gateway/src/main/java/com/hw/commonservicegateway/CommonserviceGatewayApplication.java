7439f9fa5c44c373de9980180c1db7ee648049f1
焦宏伟 <jiao.hongwei@qq.com>
2019-03-20 16:27:36 +0800

    Spring Cloud Gateway 网关

---
+++ commonservice-gateway/src/main/java/com/hw/commonservicegateway/CommonserviceGatewayApplication.java
@@ -0,0 +1,43 @@
+package com.hw.commonservicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+@RestController
+@SpringBootApplication
+public class CommonserviceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonserviceGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRouter(RouteLocatorBuilder builder) {
+        String httpUri = "http://httpbin.org:80";
+
+        return builder.routes()
+                .route(p -> p.path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+
+                /**
+                 * http://localhost:8080/delay/3
+                 * host:www.hystrix.com
+                 * 返回 fallback
+                 */
+                .route(p -> p.host("*.hystrix.com")
+                        .filters(f -> f.hystrix(config -> config.setName("mycmd").setFallbackUri("forward:/fallback")))
+                        .uri(httpUri))
+                .build();
+    }
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
+    }
+}
