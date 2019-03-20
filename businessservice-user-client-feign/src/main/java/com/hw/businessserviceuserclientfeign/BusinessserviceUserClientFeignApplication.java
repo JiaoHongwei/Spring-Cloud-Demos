5b5a8a2594a68a4fdf20adc6f245e9d3919752ae
焦宏伟 <jiao.hongwei@qq.com>
2019-03-20 15:45:47 +0800

    Hystrix Dashboard 断路器监控

--- businessservice-user-client-feign/src/main/java/com/hw/businessserviceuserclientfeign/BusinessserviceUserClientFeignApplication.java
+++ businessservice-user-client-feign/src/main/java/com/hw/businessserviceuserclientfeign/BusinessserviceUserClientFeignApplication.java
@@ -4,7 +4,9 @@ import com.hw.businessserviceuserclientfeign.service.UserManagementFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,9 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient
 @EnableFeignClients
 @RestController
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @SpringBootApplication
 public class BusinessserviceUserClientFeignApplication {
 
+//    http://localhost:8091/actuator/hystrix.stream
+
+//    localhost:8091/hystrix
     public static void main(String[] args) {
         SpringApplication.run(BusinessserviceUserClientFeignApplication.class, args);
     }
